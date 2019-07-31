e6f7e5299e09fd0ba36f0771c9d2451153f8f3ee
Burhan Muhyiddin <53507114+BurhanMuhyiddin@users.noreply.github.com>
2019-07-31 16:29:46 +0400

    This is config.h

---
+++ config.h
@@ -0,0 +1,11 @@
+#ifndef CONFIG_H
+#define	CONFIG_H
+
+#include <16F877A.h>
+#device ADC=16
+
+#FUSES NOWDT                    //No Watch Dog Timer
+#FUSES NOBROWNOUT               //No brownout reset
+#FUSES NOLVP                    //No low voltage prgming, B3(PIC16) or B5(PIC18) used for I/O
+
+#use delay(crystal=6000000)
